ff8ed94a8ed8af61e56e163a168f5bc94a0abaa7
AneezaImtiaz <41996524+AneezaImtiaz@users.noreply.github.com>
2019-06-14 19:11:03 +0500

    Update MainComponent.js

--- PhotoGalleryApp/components/MainComponent.js
+++ PhotoGalleryApp/components/MainComponent.js
@@ -4,10 +4,20 @@ import GalleryComponent from './GalleryComponent' //Import the gallery file
 import Data from '../helpers/JSON/Pictures.json'; // Import the sample data
 import {connect} from 'react-redux';
 import { getPictures } from '../redux/actions';
+import urlPaths from '../helpers/AppConfiguration';
+import axios from 'axios';
 
 class MainComponent extends Component {
 
-componentDidMount() {
+async componentDidMount() {
+
+    try {
+        // In order to get data from server, change localhost to IP address
+        const response =  await axios.get(urlPaths.galleryList);
+       
+      } catch (error) {
+        console.log(error)
+      }
     
     var _this = this;
 
@@ -16,8 +26,9 @@ componentDidMount() {
         //if it doesn't exist, extract from json file
         //save the initial data in Async
         if (data === null){
-            AsyncStorage.setItem('data', JSON.stringify(Data.pictures));
-            _this.props.getPictures();
+            
+              AsyncStorage.setItem('data', JSON.stringify(Data.pictures));
+              _this.props.getPictures();
         }
         
     });
